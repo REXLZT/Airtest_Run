655f35432e5eba8b4ce74f51dcd7d8e856cdf9af
linzeteng <linzeteng@aobi.com>
2022-10-25 10:45:42 +0800

    创建 MyStart.bat

---
+++ Bat/2/MyStart.bat
@@ -0,0 +1,21 @@
+@echo off
+:: 先打开浏览器
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" & ping localhost -n 1
+:: 接着打开QQ
+start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe "  & ping localhost -n 1
+::开说sql
+start "" "D:\Program Files (x86)\HeidiSQL\heidisql.exe"  & ping localhost -n 1
+:: sourcetree
+start "" "C:\Users\linzeteng\AppData\Local\SourceTree\SourceTree.exe"  & ping localhost -n 1
+:: cattercallshell
+start "" "C:\Users\linzeteng\AppData\Local\Programs\better-call-shell\BetterCallShell.exe"  & ping localhost -n 1
+::打开hs，很多都来不及调试
+start "" "E:\学习系列\BN\HFS\HFS\HFS\hfs.exe"  & ping localhost -n 1
+::打开Typora
+start "" "D:\bn_software\typora\Typora.exe"  & ping localhost -n 1
+::打开服务器
+start "" "C:\Program Files (x86)\Mobatek\MobaXterm\MobaXterm.exe"  & ping localhost -n 1
+
+taskkill /if /im cmd.exe
+exit
+
